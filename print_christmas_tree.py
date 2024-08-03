2a65f7ad817cfa02c5af3a5b339bb29f5c326a8f
ScheffDarthusian <148785418+Scheffdarthusian@users.noreply.github.com>
2024-08-03 16:14:04 +0900

    Update print_christmas_tree.py

--- print_christmas_tree.py
+++ print_christmas_tree.py
@@ -1,25 +1,25 @@
 def print_christmas_tree(height):
-    #Calculate the width of the tree based on the height#
-    width = 2 * height
-    #Create a list to store the rows of the tree#
+    # Calculate the width of the tree based on the height
+    width = 2 * height - 1
+    # Create a list to store the rows of the tree
     tree = []
 
-    #Add the top 'X' and '^'
-    tree.append(' ' * (width // 2) + 'X' + ' ' * (width // 2))
-    tree.append(' ' * (width // 2) + '^' + ' ' * (width // 2))
+    # Add the top 'X' and '^'
+    tree.append(' ' * ((width - 1) // 2) + 'X' + ' ' * ((width - 1) // 2))
+    tree.append(' ' * ((width - 1) // 2) + '^' + ' ' * ((width - 1) // 2))
 
-    #Add the tree body
-    for i in range(height):
-        spaces = ' ' * (height - i - 1)
+    # Add the tree body
+    for i in range(height - 1):
+        spaces = ' ' * (height - i - 2)
         stars = '*' * (2 * i + 1)
         row = spaces + '/' + stars + '\\' + spaces
         tree.append(row)
-    #Add the stem
-    stem_spaces = ' ' * (width // 2 - 1)
+
+    # Add the stem
+    stem_spaces = ' ' * ((width - 3) // 2)
     tree.append(stem_spaces + '| |' + stem_spaces)
 
     for row in tree:
         print(row)
-
 
 print_christmas_tree(int(input()))
